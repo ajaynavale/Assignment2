978b76a01f166d73192e426c642bb83a1545dfb9
Ajay <ajaynavale610@gmail.com>
2022-09-20 20:57:30 +0530

    Add project files.

---
+++ Assignment2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Assignment2
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter a mark of math subject");
+//            int m1 = Convert.ToInt32(Console.ReadLine());
+//            if(m1>100)
+//            {
+//                Console.WriteLine("Enter a valid marks");
+//            }
+
+//            Console.WriteLine("Enter a mark of Physics subject");
+//            int m2 = Convert.ToInt32(Console.ReadLine());
+//            if (m2 > 100)
+//            {
+//                Console.WriteLine("Enter a valid marks");
+//            }
+
+//            Console.WriteLine("Enter a mark of Chemistery subject");
+//            int m3 = Convert.ToInt32(Console.ReadLine());
+//            if (m3 > 100)
+//            {
+//                Console.WriteLine("Enter a valid marks");
+//            }
+
+//            int total = m1 + m2 + m3;
+//            Console.WriteLine($"Total is {total}");
+
+//            int perc = total / 3;
+//            Console.WriteLine($"Percentage is {perc}");
+
+
+//            if(perc >= 80)
+//            {
+//                Console.WriteLine("Student in Grade A");
+//            }
+//            else if( perc <= 80 && perc >= 70)
+//            {
+//                Console.WriteLine("Student in Grade B");
+//            }
+//            else if( perc <= 70 && perc >= 60)
+//            {
+//                Console.WriteLine("Student in Grade C");
+//            }
+//            else if(perc < 60)
+//            {
+//                Console.WriteLine("Student in Grade D");
+//            }
+//            //else
+//            //{
+//            //    Console.WriteLine("Student in Pass class");
+//            //}
+
+//        }
+//    }
+//}
---
+++ Assignment2/Program2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            int DA, HRA, gross;
+            Console.WriteLine("Enter a basic salary");
+            int salary = Convert.ToInt32(Console.ReadLine());
+
+            if (salary <= 10000)
+            {
+                DA = (salary * 80) / 100;
+                HRA = (salary * 20) / 100;
+                gross = salary + DA + HRA;
+                Console.WriteLine($"Gross salary is {gross}");
+            }
+            else if (salary <= 20000 )
+            {
+                DA = (salary * 90) / 100;
+                HRA = (salary * 35) / 100;
+                gross = salary + DA + HRA;
+                Console.WriteLine($"Gross salary is {gross}");
+            }
+            else
+            {
+                DA = (salary * 95) / 100;
+                HRA = (salary * 40) / 100;
+                gross = salary + DA + HRA;
+                Console.WriteLine($"Gross salary is {gross}");
+            }
+           // gross = salary + DA + HRA;
+            //Console.WriteLine($"Gross salary is {gross}");
+
+        }
+
+    }
+}
